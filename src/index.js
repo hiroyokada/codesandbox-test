e7a347225ea614071b44f570a6497b60bf017410
hiroyokada <hiroy.okada@gmail.com>
2021-12-27 17:59:11 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const testStr = "laplus";
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use ${testStr} the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
