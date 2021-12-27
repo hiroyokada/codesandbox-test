45663472998da0fc3d2e3f954070062e8ed30d1f
hiroyokada <hiroy.okada@gmail.com>
2021-12-27 18:00:54 +0900

    add test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const testStr = "laplus";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
-  We use ${testStr} the same configuration as Parcel to bundle this sandbox, you can find more
+  add We use ${testStr} the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
